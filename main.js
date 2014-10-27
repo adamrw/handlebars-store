71488356cc1186b9046f54e063fc23e654d6d4be
Adam Weiner <adamwineguy@gmail.com>
2014-10-27 13:05:12 -0600

    broke the data array

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var myStore = $('#store-template').html();
 console.log(myStore);
 
 var template = Handlebars.compile(myStore);
-	console.log(productsDataTemplate);
+	console.log(template);
 
 for (var i=0; i<productsData.productsList.length; i++){
 	var gameOutput=template(producsData.productsList[i]);
@@ -12,4 +12,20 @@ for (var i=0; i<productsData.productsList.length; i++){
 	$('#product-container').append(gameOutput);
 }
 
+var wishlistHTML = $('#wishlistitem').html();
+console.log(wishlistHTML);
+var wishTemplate = Handlebars.compile(wishlistHTML);
+
+$(document).on('click', '.add', function(){
+	var title = $(this).siblings('h2').text();
+	console.log(title);
+	var addWish = {'title': title};
+	var wishOutput = wishTemplate(addWish);
+	console.log(wishOutput);
+	$('#wishlist').append(wishOutput);
+});
+
+$(document).on('click', '.delete', function(){
+	$(this).closest('.wishItem').remove();
+});
 });
