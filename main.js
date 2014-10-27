1fe88d33f4077815c2f5c34de0063138527472c6
Adam Weiner <adamwineguy@gmail.com>
2014-10-27 12:21:45 -0600

    updated exercise

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 $(document).on('ready', function() {
   
+var myStore = $('#store-template').html();
+console.log(myStore);
+
+var template = Handlebars.compile(myStore);
+	console.log(productsDataTemplate);
+
+for (var i=0; i<productsData.productsList.length; i++){
+	var gameOutput=template(producsData.productsList[i]);
+	console.log('gameOutput: ', gameOutput);
+	$('#product-container').append(gameOutput);
+}
+
 });
